ba656cfe24b119bf4d0d9be6b6e6f408908aebc7
Sam33rRanjan <sameerranjan2003@gmail.com>
2025-02-05 12:31:14 +0530

    Update Progress

--- 01. Core Java/L12_This_and_Super_Keywords.java
+++ 01. Core Java/L12_This_and_Super_Keywords.java
@@ -1,9 +1,15 @@
 class L12_Student {
     int id;
     String name;
-
+    
+    // Default Constructor
+    L12_Student() {
+        this(0, "Default"); //constructor calling should be the first statement in the constructor
+        System.out.println("Default Constructor");
+    }
     // Parameterized Constructor
     L12_Student(int id, String name) {
+        System.out.println("Parameterized Constructor");
         this.id = id;
         this.name = name;
     }
@@ -28,7 +34,7 @@ class L12_Monitor extends L12_Student {
 }
 public class L12_This_and_Super_Keywords {
     public static void main(String[] args) {
-        L12_Student s2 = new L12_Student(1, "John");
+        L12_Student s2 = new L12_Student();
         s2.display();
 
         L12_Monitor c = new L12_Monitor(2, "Jane");
